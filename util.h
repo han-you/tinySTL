69561dbd2fbc24e67ae5d0b02e42ac7cb9c723f5
han-you <2420380374@qq.com>
2024-03-31 15:05:25 +0800

    util.h 1.0

--- util.h
+++ util.h
@@ -38,7 +38,8 @@ namespace mystl
   ForwardIter2 swap_range(ForwardIter1 first1, ForwardIter1 last, ForwardIter2 first2)
   {
     for (; first1 != last; first1++, first2++)
-      swap(*first1, *first2) return first2;
+      swap(*first1, *first2);
+    return first2;
   }
 
   template <class tp, size_t N>
@@ -61,12 +62,125 @@ namespace mystl
     template <class U1 = Ty1, class U2, typename = typename std::enable_if<std::is_copy_constructible<U1>::value && std::is_copy_constructible<U2>::value && std::is_convertible<const U1 &, Ty1>::value && std::is_convertible<const & U2, Ty2>::value, int>::type = 0>
     constexpr pair(const U1 &a, const U2 &b) : first(a), second(b) {}
 
-    template <class U1 = Ty1, class U2, typename = typename std::enable_if<std::is_copy_constructible<U1>::value && std::is_copy_constructible<U2>::value && std::is_convertible<const U1 &, Ty1>::value && std::is_convertible<const & U2, Ty2>::value, int>::type = 0>
+    template <class U1 = Ty1, class U2, typename = typename std::enable_if<std::is_copy_constructible<U1>::value && std::is_copy_constructible<U2>::value && (!std::is_convertible<const U1 &, Ty1>::value || !std::is_convertible<const & U2, Ty2>::value), int>::type = 0>
     explicit constexpr pair(const U1 &a, const U2 &b) : first(a), second(b) {}
 
     pair(const pair &) = default;
-    pair(pair &&) = default
+    pair(pair &&) = default;
+
+    template <class Other1, class Other2, typename = typename std::enable_if<std::is_constructible<Other1, Ty1>::value && std::is_constructible<Other2, Ty2>::value && std::is_convertible<Other1 &&, Ty1>::value && std::is_convertible<Other2 &&, Ty2>::value, int>::type = 0>
+    constexpr pair(Other1 &&a, Other2 &&b) : first(mystl::forward<Other1>(a)), second(mystl::forward<Other2>(b)){};
+
+    template <class Other1, class Other2, typename = typename std::enable_if<std::is_constructible<Other1, Ty1>::value && std::is_constructible<Other2, Ty2>::value && (!std::is_convertible<Other1 &&, Ty1>::value || !std::is_convertible<Other2 &&, Ty2>::value), int>::type = 0>
+    explicit constexpr pair(Other1 &&a, Other2 &&b) : first(mystl::forward<Other1>(a)), second(mystl::forward<Other2>(b)){};
+
+    template <class Other1, class Other2, typename = typename std::enable_if<std::is_constructible<const Other1 &, Ty1>::value && std::is_constructible<const Other2 &, Ty2>::value && std::is_convertible<const Other1 &, Ty1>::value && std::is_convertible<const Other2 &, Ty2>::value, int>::type = 0>
+    constexpr pair(const pair<Other1, Other2> &other) : first(other.first), second(other.second){};
+
+    template <class Other1, class Other2, typename = typename std::enable_if<std::is_constructible<const Other1 &, Ty1>::value && std::is_constructible<const Other2 &, Ty2>::value && (!std::is_convertible<const Other1 &, Ty1>::value || !std::is_convertible<const Other2 &, Ty2>::value), int>::type = 0>
+    explicit constexpr pair(const pair<Other1, Other2> &other) : first(other.first), second(other.second){};
+
+    template <class Other1, class Other2, typename = typename std::enable_if<std::is_constructible<Other1, Ty1>::value && std::is_constructible<Other2, Ty2>::value && std::is_convertible<Other1, Ty1>::value && std::is_convertible<Other2, Ty2>::value, int>::type = 0>
+    constexpr pair(pair<Otehr1, Other2> &&other) : first(mystl::forward<Other1>(other.first)), second(mystl::forward<Other2>(other.second)){};
+
+    template <class Other1, class Other2, typename = typename std::enable_if<std::is_constructible<Other1, Ty1>::value && std::is_constructible<Other2, Ty2>::value && (!std::is_convertible<Other1, Ty1>::value || !std::is_convertible<Other2, Ty2>::value), int>::type = 0>
+    explicit constexpr pair(pair<Otehr1, Other2> &&other) : first(mystl::forward<Other1>(other.first)), second(mystl::forward<Other2>(other.second)){};
+
+    // 重载运算符
+    pair &operator=(const pair &lhs)
+    {
+      if (this != &lhs)
+      {
+        this->first = lhs.first;
+        this->second = lhs.second;
+      }
+      return *this;
+    }
+
+    pair &operator=(pair &&rhs)
+    {
+      if (this != &rhs)
+      {
+        this->first = mystl::move(rhs.first);
+        this->second = mystl::move(rhs.second);
+      }
+      return *this;
+    }
+
+    template <class Otehr1, class Other2>
+    pair &operator=(const pair<Otehr1, Other2> &lhs)
+    {
+      this->first = lhs.first;
+      this->second = lhs.second;
+      return *this;
+    }
+
+    template <class Otehr1, class Other2>
+    pair &operator=(pair<Otehr1, Other2> &&rhs)
+    {
+      this->first = mystl::forward<Other1>(rhs.first);
+      this->second = mystl::forward<Other2>(rhs.first);
+      return *this;
+    }
+
+    ~pair() default;
+
+    void swap(const pair &lsh)
+    {
+      if (this != &lsh)
+      {
+        mystl::swap(first, lsh.first);
+        mystl::swap(second, lsh.second);
+      }
+    };
   };
+  template <class Ty1, class Ty2>
+  bool operator==(const pair<Ty1, Ty2> &lhs, const pair<Ty1, Ty2> &rhs)
+  {
+    return lhs.first == rhs.first && lhs.second == rhs.second;
+  }
+
+  template <class Ty1, class Ty2>
+  bool operator!=(const pair<Ty1, Ty2> &lhs, const pair<Ty1, Ty2> &rhs)
+  {
+    return !(lhs == rhs);
+  }
+
+  template <class Ty1, class Ty2>
+  bool operator<(const pair<Ty1, Ty2> &lhs, const pair<Ty1, Ty2> &rhs)
+  {
+    return lhs.first < rhs.first || (lhs.first == rhs.first && lhs.second < rhs.second);
+  }
+
+  template <class Ty1, class Ty2>
+  bool operator>(const pair<Ty1, Ty2> &lhs, const pair<Ty1, Ty2> &rhs)
+  {
+    return rhs < lhs;
+  }
+
+  template <class Ty1, class Ty2>
+  bool operator<=(const pair<Ty1, Ty2> &lhs, const pair<Ty1, Ty2> &rhs)
+  {
+    return !(rhs < lhs);
+  }
+
+  template <class Ty1, class Ty2>
+  bool operator>=(const pair<Ty1, Ty2> &lhs, const pair<Ty1, Ty2> &rhs)
+  {
+    return !(lhs < rhs);
+  }
+
+  template <class Ty1, class Ty2>
+  void swap(pair<Ty1, Ty2> &lhs, pair<Ty1, Ty2> &rhs)
+  {
+    lhs.swap(rhs);
+  }
+
+  template <class Ty1, class Ty2>
+  pair<Ty1, Ty2> make_pair(Ty1 &&first, Ty2 &&second)
+  {
+    return pair<Ty1, Ty2>(mystl::forward<Ty1>(first), mystl::forward<Ty2>(second));
+  }
 }
 
 #endif
